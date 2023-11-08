a9e4081463a71d1629c0b38e9900d7cee5a455dc
hewmx <mxhew20@gmail.com>
2023-11-08 17:46:58 +1300

    Changed URL in menu to match the new one for ghpages

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -9,7 +9,7 @@ function Menu(props) {
         <Container>
           <Navbar.Brand >Dashboard</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="/">Analytics</Nav.Link>
+            <Nav.Link href="/capstone_react_frontend">Analytics</Nav.Link>
             <Nav.Link ><Link to={"/records"} state={{date: now.slice(0, 10)}}>Records</Link></Nav.Link>
           </Nav>
         </Container>
